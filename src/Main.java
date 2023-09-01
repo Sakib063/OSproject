a8a6eb0656d64df9b90e72c63f1eab644d9dee02
wendigo <saadmansakib275@gmail.com>
2023-09-01 22:10:42 +0600

    user input hard, limited priority

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main{
     static PriorityQueue<Process>ready=new PriorityQueue<Process>(10,
@@ -30,21 +31,39 @@ public class Main{
                     return (int)(o1.getArrival_time()-o2.getArrival_time());
                 }
             });
+    static PriorityQueue<Process>executeq4=new PriorityQueue<Process>(10,
+            new Comparator<Process>() {
+                @Override
+                public int compare(Process o1, Process o2){
+                    return (int)(o1.getArrival_time()-o2.getArrival_time());
+                }
+            });
     static GlobalTimer globalTimer=new GlobalTimer(0);
     static void run_cpu(PriorityQueue<Process> p){
         Process process=p.poll();
         process.run_process();
     }
     public static void main(String[] args){
-        ready.add(new Process(1,1,5,1,globalTimer));
-        ready.add(new Process(2,2,7,2,globalTimer));
-        ready.add(new Process(3,3,3,3,globalTimer));
-        ready.add(new Process(4,3,2,5,globalTimer));
-        ready.add(new Process(5,2,10,4,globalTimer));
-        ready.add(new Process(6,1,11,4,globalTimer));
-
+        int time=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of processes:");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.println("Process ID:"+i);
+            System.out.println("Priority must be between 1 to 4");
+            System.out.println("Priority:");
+            int pri= sc.nextInt();
+            System.out.println("Process ID:"+i);
+            System.out.println("Arrival Time:");
+            int at= sc.nextInt();
+            System.out.println("Process ID:"+i);
+            System.out.println("Burst Time:");
+            int bt= sc.nextInt();
+            ready.add(new Process(i,pri,at,bt,globalTimer));
+        }
 
         while(!ready.isEmpty()){
+            time=time+ready.element().burst_time;
             if(ready.element().getPriority()==1){
                 executeq1.add(ready.poll());
             }
@@ -54,8 +73,11 @@ public class Main{
             else if(ready.element().getPriority()==3){
                 executeq3.add(ready.poll());
             }
+            else if(ready.element().getPriority()==4){
+                executeq4.add(ready.poll());
+            }
         }
-        while(globalTimer.time<=50){
+        while(globalTimer.time<=time){
             if(!executeq3.isEmpty()){
                 run_cpu(executeq3);
                 globalTimer.time++;
